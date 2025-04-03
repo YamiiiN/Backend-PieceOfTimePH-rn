88615493576c865ed02b0962246b25dcc789149b
Minazuna <159107536+Minazuna@users.noreply.github.com>
2025-04-03 20:27:20 +0800

    order update method

--- controllers/order.js
+++ controllers/order.js
@@ -28,7 +28,7 @@ exports.create = async (req, res, next) => {
 };
 exports.getUserOrders = async (req, res) => {
     try {
-        const userId = req.user._id; // Assuming req.user is set after authentication
+        const userId = req.user._id; 
         const orders = await Order.find({ user: userId }).populate({
             path: 'order_items.product',
             select: 'name sell_price images',
@@ -72,3 +72,47 @@ exports.all = async (req, res, next) => {
 
     }
 }
+
+exports.updateOrder = async (req, res) => {
+    try {
+      const { id } = req.params;
+      const { status } = req.body;
+      
+      const order = await Order.findById(id);
+      
+      if (!order) {
+        return res.status(404).json({
+          success: false,
+          message: 'Order not found'
+        });
+      }
+      
+      order.status = status;
+      await order.save();
+      
+      // return the updated order
+      const updatedOrder = await Order.findById(id)
+        .populate({
+          path: 'order_items.product',
+          select: 'name images quantity sell_price'
+        })
+        .populate({
+          path: 'user',
+          select: 'first_name last_name email images'
+        });
+      
+      res.status(200).json({
+        success: true,
+        message: 'Order status updated successfully',
+        order: updatedOrder
+      });
+      
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({
+        success: false,
+        message: 'Failed to update order status',
+        error: error.message
+      });
+    }
+  };
--- routes/order.js
+++ routes/order.js
@@ -1,18 +1,18 @@
 const express = require('express');
 const router = express.Router();
-const { isAuthenticated, isAuthenticatedV2 } = require('../middleware/auth');
+const { isAuthenticated, isAuthenticatedV2, authorizeRoles } = require('../middleware/auth');
 
 const {
     getUserOrders, 
     create, 
     all,
+    updateOrder
 } = require("../controllers/order")
 
 
 router.post('/create', isAuthenticated, create);
-
-router.get('/all', isAuthenticated, all);
-
-router.get('/user/orders', isAuthenticated, getUserOrders);
+router.get('/all', isAuthenticated, authorizeRoles('admin'), all);
+router.get('/user/orders', isAuthenticated, authorizeRoles('user'), getUserOrders);
+router.put('/:id', isAuthenticated, authorizeRoles('admin'), updateOrder);
 
 module.exports = router;
